ad839897e8b2d156a1b80c3afb4d62b53175112c
krish-gupta53 <krish.fig00@gmail.com>
2026-06-16 18:12:11 +0530

    Reload workspace after successful sign in

---
+++ src/workspace-ui-script-l.js
@@ -0,0 +1,3 @@
+export const WORKSPACE_SCRIPT_L = String.raw`
+var loginForm=document.getElementById('loginForm');if(loginForm)loginForm.addEventListener('submit',function(){setTimeout(function(){location.reload()},800)});
+`;
